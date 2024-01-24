34f6962c088a4922f457fa4a79e6291f8058d5ec
jung <mg1997@naver.com>
2024-01-24 12:35:49 +0900

    initial commit

--- src/main/java/com/min/myblogv1/controller/api/textApiController.java
+++ src/main/java/com/min/myblogv1/controller/api/textApiController.java
@@ -25,7 +25,6 @@ public class textApiController {
         log.info("임시파일 로직 ={}",file.getName());
         log.info("임시파일 로직 ={}",file.getContentType());
         log.info("임시파일 로직 ={}",file.getInputStream());
-        log.info("임시파일 로직 ={}",file.getBytes());
         log.info("임시파일 로직 ={}",file.getResource());
         return new ResponseEntity<>(file.toString(), HttpStatus.OK);
     }
