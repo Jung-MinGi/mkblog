92cc493b5de5094877417e0d95b4133071cc327a
jung <mg1997@naver.com>
2024-01-24 12:27:02 +0900

    initial commit

---
+++ src/main/java/com/min/myblogv1/controller/api/textApiController.java
@@ -0,0 +1,33 @@
+package com.min.myblogv1.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.asm.TypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class textApiController {
+    private final ObjectMapper objectMapper;
+    @PostMapping("/temp/upload")
+    public ResponseEntity<String> tempImageSave(MultipartFile file) throws IOException {
+
+        log.info("임시파일 로직 ={}",file.getOriginalFilename());
+        log.info("임시파일 로직 ={}",file.getName());
+        log.info("임시파일 로직 ={}",file.getContentType());
+        log.info("임시파일 로직 ={}",file.getInputStream());
+        log.info("임시파일 로직 ={}",file.getBytes());
+        log.info("임시파일 로직 ={}",file.getResource());
+        return new ResponseEntity<>(file.toString(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/min/myblogv1/controller/viewController.java
@@ -0,0 +1,23 @@
+package com.min.myblogv1.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class viewController {
+
+    @GetMapping("/image")//글 작성 페이지로 이동 핸들러
+    public String summer(Model model) {
+//        List<String> list = service.getTables();
+//        log.info("show tables={}", list);
+//        model.addAttribute("tables", list);
+        return "board";
+    }
+}
